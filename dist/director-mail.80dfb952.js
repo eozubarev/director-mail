ff82c4f0b829bd998174165bee569875cdf96af2
“Evgeny <ahotickkk@mail.ru>
2021-02-17 13:46:16 +0300

    edit mobile responsive

--- dist/director-mail.80dfb952.js
+++ dist/director-mail.80dfb952.js
@@ -1144,7 +1144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63185" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63886" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
